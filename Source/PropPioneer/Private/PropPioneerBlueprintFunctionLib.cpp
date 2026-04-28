52e2ea8f81d7862a08032ee99a0a0ee5200ce6f1
Jesse Hodgson <space@spacegamedev.com>
2026-04-28 13:21:29 -0400

    v1.1.0, Character Replacer Support

---
+++ Source/PropPioneer/Private/PropPioneerBlueprintFunctionLib.cpp
@@ -0,0 +1,2 @@
+
+#include "PropPioneerBlueprintFunctionLib.h"
---
+++ Source/PropPioneer/Public/PropPioneerBlueprintFunctionLib.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "PropPioneerBlueprintFunctionLib.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROPPIONEER_API UPropPioneerBlueprintFunctionLib : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+};
